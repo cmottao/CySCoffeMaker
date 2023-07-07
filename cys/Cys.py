9899f43dec143634e0793e12697588b9192e2c1c
cmottao <cristianstevenmottaojeda@gmail.com>
2023-07-06 19:42:11 -0500

    version 1.1

--- cys/Cys.py
+++ cys/Cys.py
@@ -2,68 +2,88 @@ from CoffeeMaker import CoffeeMaker
 from Recipe import Recipe
 from Screen import Screen
 
-class Cys:
+class Cys(CoffeeMaker):
     '''Represents the CyS CoffeeMaker.'''
 
-    # Methods 
-    def turn_on(self):
-        '''Turn on the CyS CoffeeMaker.'''
+    # Constructor method
+    def __init__(self):
+        '''Initializes the CyS CoffeeMaker.'''
 
-        # Coffee recipes are created using the Recipe class    
-        recipes = {
+        super().__init__()
+
+        # Coffee recipes are created using the Recipe class  
+        self.recipes = {
             'E': Recipe('Espresso', 7, 30, 25),
             'C': Recipe('Capuccino', 7, 30, 30, 125, 4),
             'L': Recipe('Latte Macciato', 7, 30, 35, 300),
             'A': Recipe('American', 14, 60, 20)
         }
-            
-        # The CoffeeMaker class is instantiated to create the CyS CoffeeMaker
-        cys = CoffeeMaker()
 
         # The Screen class is instantiated to create the "screen" of the CyS CoffeeMaker
-        screen = Screen()
-        
-        # Language input
+        self.screen = Screen()
+
+    # Methods 
+    def input_language(self):
+        '''Prompts the user to choose the language for the CoffeeMaker's screen.'''
+
         while True:
-            screen.display_message('choose_language', 'E')
-            language = input('>>> ')
-            if language == 'E' or language == 'S':
+            self.screen.display_message('choose_language', 'E')
+            self.language = input('>>> ')
+            if self.language == 'E' or self.language == 'S':
                 break
-            screen.display_message('invalid_language', 'E')
-            screen.wait(4)
+            self.screen.display_message('invalid_language', 'E')
+            self.screen.wait(4)
 
-        screen.display_message('welcome', language)
-        screen.wait(4)
+        self.screen.display_message('welcome', self.language)
+        self.screen.wait(4)
 
-        # Menu
+    def option_make_coffee(self, user_choice):
+        '''Processes the selected coffee option.'''
+
+        if self.enough_amount(self.recipes[user_choice]): # Checks if there are enough ingredients
+            self.make_coffee(self.recipes[user_choice]) # Makes the selected coffee
+            self.screen.chronometer(self.recipes[user_choice].get_time()) # Chronometer
+            self.screen.display_done_message(self.recipes[user_choice], self.language)
+        else:
+            self.screen.display_message('not_enough_ingredients', self.language)
+
+    def option_refill(self):
+        '''Refills the deposit of ingredients in the CoffeeMaker.'''
+        
+        if self.full(): # If the deposit is full, it is not necessary to refill
+            self.screen.display_message('is_full', self.language)
+        else:
+            self.refill() # Refills the deposit
+            self.screen.display_message('filled_in', self.language)
+
+    def main_menu_loop(self):
+        '''Displays the main menu and handles user input until the user chooses to exit.'''
+
         while True:          
-            screen.display_message('menu', language)
+            self.screen.display_message('menu', self.language)
             user_choice = input('>>> ')
 
             if user_choice in ('E','C','L','A'): # Options to make coffee
-                if cys.enough_amount(recipes[user_choice]): # Checks if there are enough ingredients
-                    cys.make_coffee(recipes[user_choice]) # Makes the selected coffee
-                    screen.chronometer(recipes[user_choice].get_time()) # Chronometer
-                    screen.display_done_message(recipes[user_choice], language)
-                else:
-                    screen.display_message('not_enough_ingredients', language)
-                        
+                self.option_make_coffee(user_choice)
+                       
             elif user_choice == 'R': # Option to refill deposit
-                if cys.full(): # If the deposit is full, it is not necessary to refill
-                    screen.display_message('is_full', language)
-                else:
-                    cys.refill() # Refills the deposit
-                    screen.display_message('filled_in', language)
+                self.option_refill()
                             
             elif user_choice == 'S': # Option to show the status of the deposit
-                screen.display_deposit_message(cys, language)
+                self.screen.display_deposit_message(self, self.language)
                             
             elif user_choice == 'X': # Option to exit
-                screen.display_message('farewell', language)
-                screen.wait(4)
+                self.screen.display_message('farewell', self.language)
+                self.screen.wait(4)
                 break
 
             else: # Invalid option
-                screen.display_message('invalid_option', language)
+                self.screen.display_message('invalid_option', self.language)
                 
-            screen.wait(4)
+            self.screen.wait(4)
+
+    def turn_on(self):
+        '''Turns on the CyS CoffeeMaker.'''
+
+        self.input_language()
+        self.main_menu_loop()
